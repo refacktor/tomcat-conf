996faef93008c24904c37e8058d6b4f00b6cb078
root <root@ctcdev>
2015-12-12 20:29:57 -0600

    JdbcHandler configured ok

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
 mvn install
 cp target/tomcat7-conf-1.0-RELEASE.jar /usr/share/tomcat7/lib/
+
+for i in /usr/share/tomcat7/lib/*.jar
+do
+ list=$i:$list
+done
+
+cat >/usr/share/tomcat7/bin/setenv.sh <<EOF
+CLASSPATH=\
+/usr/share/tomcat7/lib/tomcat7-conf-1.0-RELEASE.jar:\
+/usr/share/java/mysql-connector-java.jar:\
+/usr/share/tomcat7/bin/tomcat-juli.jar\
+$list
+EOF
+
